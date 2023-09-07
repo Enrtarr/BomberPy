2500beb645184f2ce1e71ab305320ff48aa49fbe
Enrtarr <120576263+Enrtarr@users.noreply.github.com>
2023-09-07 17:16:34 +0200

    Added input choice menu
    - rajoute un menu pour choisir ses touches
    - résolution d'un bug qui rendait le redémarage de l'app nécessaire afin d'appliquer les paramètres

--- bbm/bbm_menu.py
+++ bbm/bbm_menu.py
@@ -6,13 +6,18 @@ from ursina import *
 # import datamanager
 import splashscreen
 
-# app = Ursina(development_mode=False)
-app=Ursina()
+app = Ursina(development_mode=True)
 
 
 # application.paused = True
 # SC_duration = 4
-# splashscreen.SplashScreen(overlay_color=color.white,logo_texture='./textures/logo_aa.png',delay_time=SC_duration,audio='./audio/crazy_frog.mp3',audio_volume=1)
+# splashscreen.SplashScreen(
+#     overlay_color=color.white,
+#     logo_texture='./textures/logo_aa.png',
+#     delay_time=SC_duration,
+#     audio='./audio/crazy_frog.mp3',
+#     audio_volume=1
+#     )
 # invoke(setattr, application, 'paused', False, delay=1.2*SC_duration)
 
 
@@ -25,6 +30,32 @@ class MenuButton(Button):
 
         for key, value in kwargs.items():
             setattr(self, key ,value)
+
+# class InputChoice(InputField):
+#         def __init__(self, default_v, key, **kwargs):
+#             super().__init__(default_v, key, **kwargs)
+            
+#             # self.parent = input_menu
+            
+#             self.limit_content_to = 'abcdefghijklmnopqrstuvwxyz'
+#             self.default_value = default_v
+#             self.key = key
+            
+#             # self.submit_button = Button(
+#             #     'Appliquer', 
+#             #     on_click=self.submit, 
+#             #     x=self.x-.26,
+#             #     scale=(.25, .075), 
+#             #     highlight_color=color.red,
+#             #     parent=self
+#             #     )
+#             # self.submit_button.fit_to_text()
+
+#             for key, value in kwargs.items():
+#                 setattr(self, key ,value)
+        
+#         def submit(self):
+#             settings_file["Inputs"][self.key] = self.text
 
 # button_size = (.25, .075)
 button_spacing = .075 * 1.25
@@ -58,12 +89,18 @@ def start_game():
     menu_parent.enabled = False
     import bbm
     bbm.input_mode = input_mode
+    # bbm.atk_key = 
 
 # charge le contenu du menu jouer
 for i in range(3):
     MenuButton(parent=load_menu, text=f'Emplacement vide {i}', y=-i * button_spacing, on_click=start_game)
 
-load_menu.back_button = MenuButton(parent=load_menu, text='Retour', y=((-i-2) * button_spacing), on_click=Func(setattr, state_handler, 'state', 'main_menu'))
+load_menu.back_button = MenuButton(
+    parent=load_menu, 
+    text='Retour', 
+    y=((-i-2) * button_spacing), 
+    on_click=Func(setattr, state_handler, 'state', 'main_menu')
+    )
 
 
 # options menu content
@@ -72,10 +109,20 @@ preview_text = Text(parent=option_menu, x=.275, y=.25, text='Texte d\'exemple', 
 #     t.original_scale = t.scale
     
 options = []
+inputs = []
 
 
 text_scale_value = settings_file["TextSize"]
-text_scale_slider = Slider(0, 2, default=text_scale_value, step=.1, dynamic=True, text='Taille du texte :', parent=option_menu, x=-.25)
+text_scale_slider = Slider(
+    0, 
+    2, 
+    default=text_scale_value, 
+    step=.1, 
+    dynamic=True, 
+    text='Taille du texte :', 
+    parent=option_menu, 
+    x=-.25
+    )
 
 def set_text_scale():
     for t in [e for e in scene.entities if isinstance(e, Text) and hasattr(e, 'original_scale')]:
@@ -102,10 +149,10 @@ def set_input_mode():
         settings_file["InputMode"] = 'Manette'
     else:
         settings_file["InputMode"] = 'Clavier'
-    input_button.text = settings_file["InputMode"]
+    input_mode_button.text = settings_file["InputMode"]
 
 input_mode = settings_file["InputMode"]
-input_button = Button(
+input_mode_button = Button(
     parent=option_menu, 
     text=input_mode, 
     scale=(.25, .075), 
@@ -115,36 +162,85 @@ input_button = Button(
     )
 input_mode_text = Text(parent=option_menu, text="Mode d'entrée", origin=(0,0), x=-.125, color=color.white)
 
-options.append(input_button)
+options.append(input_mode_button)
 
 input_menu_button = MenuButton(parent=option_menu, text='Commandes', on_click=Func(setattr, state_handler, 'state', 'input_menu'))
 options.append(input_menu_button)
 
+atk_key = settings_file["Inputs"]["atk"]
+pause_key = settings_file["Inputs"]["pause"]
+
 def apply():
     settings_file["TextSize"] = round(text_scale_slider.value, 1)
     settings_file["Volume"] = round(volume_slider.value, 1)
-    settings_file["InputMode"] = input_button.text
+    settings_file["InputMode"] = input_mode_button.text
+    
+    settings_file["Inputs"]["pause"] = inputs_panel.content[1].text[0]
+    settings_file["Inputs"]["atk"] = inputs_panel.content[3].text[0]
+    
+    global input_mode, atk_key, pause_key
+    input_mode = input_mode_button.text
+    atk_key = inputs_panel.content[1].text[0]
+    pause_key = inputs_panel.content[3].text[0]
     
     with open('./datas/settings.json', 'w') as ecriveur:
         json.dump(settings_file, ecriveur, indent=4)
     
 
-apply_button = MenuButton(parent=option_menu, text='Appliquer', x=0, origin_x=-.5, on_click=Func(apply))
-options_back = MenuButton(parent=option_menu, text='Retour', x=-.25, origin_x=-.5, on_click=Func(setattr, state_handler, 'state', 'main_menu'))
+apply_button = MenuButton(
+    parent=option_menu, 
+    text='Appliquer', 
+    x=0, 
+    origin_x=-.5, 
+    on_click=Func(apply)
+    )
+options_back = MenuButton(
+    parent=option_menu, 
+    text='Retour', 
+    x=-.25, 
+    origin_x=-.5, 
+    on_click=Func(setattr, state_handler, 'state', 'main_menu')
+    )
 options.append(options_back)
+
+
+input_back = MenuButton(
+    parent=input_menu, 
+    text='Retour', 
+    x=-.25, 
+    origin_x=-.5, 
+    on_click=Func(setattr, state_handler, 'state', 'option_menu')
+    )
+inputs.append(input_back)
+
+# input_atk_button = InputChoice(default_v=settings_file["Inputs"]["atk"], key="atk", parent=input_menu)
+inputs_panel = WindowPanel(
+    title='Contrôles',
+    content=(
+        Text('Pause :'),
+        InputField(default_value=settings_file["Inputs"]["pause"], limit_content_to='abcdefghijklmnopqrstuvwxyz'),
+        Text('Attaque :'), 
+        InputField(default_value=settings_file["Inputs"]["atk"], limit_content_to='abcdefghijklmnopqrstuvwxyz'),
+        ),
+    parent=input_menu,
+    lock=Vec3(1,1,1),
+    )
+# inputs.append(input_atk_button)
+inputs.append(inputs_panel)
 
 
 for i, e in enumerate(options):
     e.y = -i * button_spacing
 apply_button.y = options_back.y
-input_mode_text.y = input_button.y
+input_mode_text.y = input_mode_button.y
+
+for i, e in enumerate(inputs):
+    e.y = -i * button_spacing
+    print(e)
 
 
 for t in [e for e in scene.entities if isinstance(e, Text)]:
     t.original_scale = t.scale
-    
-
-input_back = MenuButton(parent=input_menu, text='Retour', x=-.25, origin_x=-.5, on_click=Func(setattr, state_handler, 'state', 'option_menu'))
 
 
 # animate the buttons in nicely when changing menu
