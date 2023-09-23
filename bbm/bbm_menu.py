3d965869d68356ed9c05b97b65a288f15d8d317a
Enrtarr <120576263+Enrtarr@users.noreply.github.com>
2023-09-23 12:43:29 +0200

    Update bbm_menu.py
    Changed wildcard import of ursina to normal import

--- bbm/bbm_menu.py
+++ bbm/bbm_menu.py
@@ -1,35 +1,36 @@
 
 import json
 
-from ursina import *
+import ursina as urs
 
 # import datamanager
 import splashscreen
 
-app = Ursina(development_mode=True)
+app = urs.Ursina(development_mode=True)
 
 
-# application.paused = True
-# SC_duration = 4
-# splashscreen.SplashScreen(
-#     overlay_color=color.white,
-#     logo_texture='./textures/logo_aa.png',
-#     delay_time=SC_duration,
-#     audio='./audio/crazy_frog.mp3',
-#     audio_volume=1
-#     )
-# invoke(setattr, application, 'paused', False, delay=1.2*SC_duration)
+urs.application.paused = True
+SC_duration = 4
+splashscreen.SplashScreen(
+    overlay_color=urs.color.white,
+    logo_texture='./textures/logo_aa.png',
+    delay_time=SC_duration,
+    audio='./audio/crazy_frog.mp3',
+    audio_volume=1
+    )
+urs.invoke(setattr, urs.application, 'paused', False, delay=1.2*SC_duration)
 
 
 with open('./datas/settings.json', 'r') as liseur:
     settings_file = json.load(liseur)
 
-class MenuButton(Button):
+class MenuButton(urs.Button):
     def __init__(self, text='', **kwargs):
-        super().__init__(text, scale=(.25, .075), highlight_color=color.red, **kwargs)
+        super().__init__(text, scale=(.25, .075), highlight_color=urs.color.red, **kwargs)
 
         for key, value in kwargs.items():
             setattr(self, key ,value)
+
 
 # class InputChoice(InputField):
 #         def __init__(self, default_v, key, **kwargs):
@@ -59,13 +60,13 @@ class MenuButton(Button):
 
 # button_size = (.25, .075)
 button_spacing = .075 * 1.25
-menu_parent = Entity(parent=camera.ui, y=.15)
-main_menu = Entity(parent=menu_parent)
-load_menu = Entity(parent=menu_parent)
-option_menu = Entity(parent=menu_parent)
-input_menu = Entity(parent=menu_parent)
+menu_parent = urs.Entity(parent=urs.camera.ui, y=.15)
+main_menu = urs.Entity(parent=menu_parent)
+load_menu = urs.Entity(parent=menu_parent)
+option_menu = urs.Entity(parent=menu_parent)
+input_menu = urs.Entity(parent=menu_parent)
 
-state_handler = Animator({
+state_handler = urs.Animator({
     'main_menu' : main_menu,
     'load_menu' : load_menu,
     'option_menu' : option_menu,
@@ -75,9 +76,9 @@ state_handler = Animator({
 
 # main menu content
 main_menu.buttons = [
-    MenuButton('Jouer', on_click=Func(setattr, state_handler, 'state', 'load_menu')),
-    MenuButton('Options', on_click=Func(setattr, state_handler, 'state', 'option_menu')),
-    MenuButton('Quitter', on_click=Sequence(Wait(.01), Func(application.quit))),
+    MenuButton('Jouer', on_click=urs.Func(setattr, state_handler, 'state', 'load_menu')),
+    MenuButton('Options', on_click=urs.Func(setattr, state_handler, 'state', 'option_menu')),
+    MenuButton('Quitter', on_click=urs.Sequence(urs.Wait(.01), urs.Func(urs.application.quit))),
 ]
 # reparent les boutons principaux au menu principal
 for i, e in enumerate(main_menu.buttons):
@@ -99,12 +100,12 @@ load_menu.back_button = MenuButton(
     parent=load_menu, 
     text='Retour', 
     y=((-i-2) * button_spacing), 
-    on_click=Func(setattr, state_handler, 'state', 'main_menu')
+    on_click=urs.Func(setattr, state_handler, 'state', 'main_menu')
     )
 
 
 # options menu content
-preview_text = Text(parent=option_menu, x=.275, y=.25, text='Texte d\'exemple', origin=(-.5,0))
+preview_text = urs.Text(parent=option_menu, x=.275, y=.25, text='Texte d\'exemple', origin=(-.5,0))
 # for t in [e for e in scene.entities if isinstance(e, Text)]:
 #     t.original_scale = t.scale
     
@@ -113,7 +114,7 @@ inputs = []
 
 
 text_scale_value = settings_file["TextSize"]
-text_scale_slider = Slider(
+text_scale_slider = urs.Slider(
     0, 
     2, 
     default=text_scale_value, 
@@ -125,7 +126,7 @@ text_scale_slider = Slider(
     )
 
 def set_text_scale():
-    for t in [e for e in scene.entities if isinstance(e, Text) and hasattr(e, 'original_scale')]:
+    for t in [e for e in urs.scene.entities if isinstance(e, urs.Text) and hasattr(e, 'original_scale')]:
         t.scale = t.original_scale * text_scale_slider.value
 text_scale_slider.on_value_changed = set_text_scale
 # appelle la fonction afin de mettre le texte à la bonne taille
@@ -135,10 +136,10 @@ options.append(text_scale_slider)
 
 
 audio_volume_value = settings_file["Volume"]
-volume_slider = Slider(0, 2, default=audio_volume_value, step=.1, text='Volume principal :', parent=option_menu, x=-.25)
+volume_slider = urs.Slider(0, 2, default=audio_volume_value, step=.1, text='Volume principal :', parent=option_menu, x=-.25)
 
 def set_volume_multiplier():
-    Audio.volume_multiplier = volume_slider.value
+    urs.Audio.volume_multiplier = volume_slider.value
 volume_slider.on_value_changed = set_volume_multiplier
 
 options.append(volume_slider)
@@ -152,19 +153,19 @@ def set_input_mode():
     input_mode_button.text = settings_file["InputMode"]
 
 input_mode = settings_file["InputMode"]
-input_mode_button = Button(
+input_mode_button = urs.Button(
     parent=option_menu, 
     text=input_mode, 
     scale=(.25, .075), 
-    highlight_color=color.red, 
-    on_click=Func(set_input_mode),
+    highlight_color=urs.color.red, 
+    on_click=urs.Func(set_input_mode),
     x=.125
     )
-input_mode_text = Text(parent=option_menu, text="Mode d'entrée", origin=(0,0), x=-.125, color=color.white)
+input_mode_text = urs.Text(parent=option_menu, text="Mode d'entrée", origin=(0,0), x=-.125, color=urs.color.white)
 
 options.append(input_mode_button)
 
-input_menu_button = MenuButton(parent=option_menu, text='Commandes', on_click=Func(setattr, state_handler, 'state', 'input_menu'))
+input_menu_button = MenuButton(parent=option_menu, text='Commandes', on_click=urs.Func(setattr, state_handler, 'state', 'input_menu'))
 options.append(input_menu_button)
 
 atk_key = settings_file["Inputs"]["atk"]
@@ -175,13 +176,13 @@ def apply():
     settings_file["Volume"] = round(volume_slider.value, 1)
     settings_file["InputMode"] = input_mode_button.text
     
-    settings_file["Inputs"]["pause"] = inputs_panel.content[1].text[0]
-    settings_file["Inputs"]["atk"] = inputs_panel.content[3].text[0]
+    # settings_file["Inputs"]["pause"] = inputs_panel.content[1].text[0]
+    # settings_file["Inputs"]["atk"] = inputs_panel.content[3].text[0]
     
     global input_mode, atk_key, pause_key
     input_mode = input_mode_button.text
-    atk_key = inputs_panel.content[1].text[0]
-    pause_key = inputs_panel.content[3].text[0]
+    # atk_key = inputs_panel.content[1].text[0]
+    # pause_key = inputs_panel.content[3].text[0]
     
     with open('./datas/settings.json', 'w') as ecriveur:
         json.dump(settings_file, ecriveur, indent=4)
@@ -192,14 +193,14 @@ apply_button = MenuButton(
     text='Appliquer', 
     x=0, 
     origin_x=-.5, 
-    on_click=Func(apply)
+    on_click=urs.Func(apply)
     )
 options_back = MenuButton(
     parent=option_menu, 
     text='Retour', 
     x=-.25, 
     origin_x=-.5, 
-    on_click=Func(setattr, state_handler, 'state', 'main_menu')
+    on_click=urs.Func(setattr, state_handler, 'state', 'main_menu')
     )
 options.append(options_back)
 
@@ -209,24 +210,24 @@ input_back = MenuButton(
     text='Retour', 
     x=-.25, 
     origin_x=-.5, 
-    on_click=Func(setattr, state_handler, 'state', 'option_menu')
+    on_click=urs.Func(setattr, state_handler, 'state', 'option_menu')
     )
 inputs.append(input_back)
 
 # input_atk_button = InputChoice(default_v=settings_file["Inputs"]["atk"], key="atk", parent=input_menu)
-inputs_panel = WindowPanel(
-    title='Contrôles',
-    content=(
-        Text('Pause :'),
-        InputField(default_value=settings_file["Inputs"]["pause"], limit_content_to='abcdefghijklmnopqrstuvwxyz'),
-        Text('Attaque :'), 
-        InputField(default_value=settings_file["Inputs"]["atk"], limit_content_to='abcdefghijklmnopqrstuvwxyz'),
-        ),
-    parent=input_menu,
-    lock=Vec3(1,1,1),
-    )
+# inputs_panel = WindowPanel(
+#     title='Contrôles',
+#     content=(
+#         Text('Pause :'),
+#         InputField(default_value=settings_file["Inputs"]["pause"], limit_content_to='abcdefghijklmnopqrstuvwxyz'),
+#         Text('Attaque :'), 
+#         InputField(default_value=settings_file["Inputs"]["atk"], limit_content_to='abcdefghijklmnopqrstuvwxyz'),
+#         ),
+#     parent=input_menu,
+#     lock=Vec3(1,1,1),
+#     )
 # inputs.append(input_atk_button)
-inputs.append(inputs_panel)
+# inputs.append(inputs_panel)
 
 
 for i, e in enumerate(options):
@@ -239,7 +240,7 @@ for i, e in enumerate(inputs):
     print(e)
 
 
-for t in [e for e in scene.entities if isinstance(e, Text)]:
+for t in [e for e in urs.scene.entities if isinstance(e, urs.Text)]:
     t.original_scale = t.scale
 
 
@@ -249,10 +250,10 @@ for menu in (main_menu, load_menu, option_menu, input_menu):
         for i, e in enumerate(menu.children):
             e.original_x = e.x
             e.x += .1
-            e.animate_x(e.original_x, delay=i*.05, duration=.1, curve=curve.out_quad)
+            e.animate_x(e.original_x, delay=i*.05, duration=.1, curve=urs.curve.out_quad)
 
             e.alpha = 0
-            e.animate('alpha', .7, delay=i*.05, duration=.1, curve=curve.out_quad)
+            e.animate('alpha', .7, delay=i*.05, duration=.1, curve=urs.curve.out_quad)
 
             if hasattr(e, 'text_entity'):
                 e.text_entity.alpha = 0
@@ -261,6 +262,13 @@ for menu in (main_menu, load_menu, option_menu, input_menu):
     menu.on_enable = animate_in_menu
 
 
-background = Entity(parent=menu_parent, model='quad', texture='./textures/bbm_bg1', scale=(camera.aspect_ratio,1), color=color.white, z=1, world_y=0)
+background = urs.Entity(
+    parent=menu_parent, 
+    model='quad', 
+    texture='./textures/bbm_bg1', 
+    scale=(urs.camera.aspect_ratio,1), 
+    color=urs.color.white, 
+    z=1, 
+    world_y=0)
 
 app.run()
